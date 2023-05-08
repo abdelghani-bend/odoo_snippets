b7cfd1e24c47641e849639e29be2680d2d8eab8b
Abdelghani BENYAMINA DOUMA <abgh.bend98@gmail.com>
2023-05-08 19:02:45 +0100

    [ADD] add structures projects

---
+++ my_modules/odoo_snippets/__manifest__.py
@@ -0,0 +1,33 @@
+#############################################################
+# AUTHOR: Abdelghani BENYAMINA DOUMA
+# COPYRIGHT: 2022 | WELLSOFT
+#############################################################
+
+{
+    'name': 'Odoo snippets',
+    'summary': 'Well odoo snippets',
+    'version': '15.0.1.0.1', 
+    'category': 'WELLSOFT/Odoo snippets',
+    'license': 'LGPL-3', 
+    'author': 'WELLSOFT',
+    'website': 'https://www.linkedin.com/in/abdelghani-bend',
+    'contributors': [],
+    'depends': [],
+    'excludes': [],
+    'data': [
+        # datas
+        # security
+        # views
+        # menus
+    ],
+    'assets': {},
+    'images': [],
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'uninstall_hook': False,
+}
